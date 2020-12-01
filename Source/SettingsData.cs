4fc2a493527233f26921da0e5dccc815dcb0f205
Squirting Elephant <squirtingelephant@gmail.com>
2020-12-01 16:36:09 +0100

    Initial commit

---
+++ Source/SettingsData.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace SquirtingElephant.ConfigurableBiocoding
+{
+    public class SettingsData : ModSettings
+    {
+        public List<BiocodeData> BiocodeData = null;
+
+        /// <summary>
+        /// The Biocode data to use for everything else.
+        /// </summary>
+        public BiocodeData BCD_Other = null;
+
+        public override void ExposeData()
+        {
+            if (BiocodeData == null) { CreateBiocodeData(); }
+
+            BiocodeData.ForEach(d => d.ExposeData());
+            BCD_Other.ExposeData();
+        }
+
+        public void CreateBiocodeData()
+        {
+            BiocodeData = new List<BiocodeData>()
+            {
+                // Mercenaries from PawnKinds_Mercenary.xml
+                new BiocodeData("Grenadier_Destructive", "SECBC_Grenadier_Destructive"),
+                new BiocodeData("Grenadier_EMP", "SECBC_Grenadier_EMP"),
+                new BiocodeData("Grenadier_Smoke", "SECBC_Grenadier_Smoke"),
+
+                new BiocodeData("Mercenary_Gunner", "SECBC_Mercenary_Gunner"),
+                new BiocodeData("Mercenary_Sniper", "SECBC_Mercenary_Sniper"),
+                new BiocodeData("Mercenary_Slasher", "SECBC_Mercenary_Slasher"),
+                new BiocodeData("Mercenary_Heavy", "SECBC_Mercenary_Heavy"),
+                new BiocodeData("PirateBoss", "SECBC_PirateBoss"),
+                new BiocodeData("Mercenary_Elite", "SECBC_Mercenary_Elite"),
+                
+                // Pirates from PawnKinds_Pirate.xml
+                new BiocodeData("Drifter", "SECBC_Drifter"),
+                new BiocodeData("Scavenger", "SECBC_Scavenger"),
+                new BiocodeData("Thrasher", "SECBC_Thrasher"),
+                new BiocodeData("Pirate", "SECBC_Pirate"),
+
+                // Spacers from PawnKinds_Spacer.xml
+                new BiocodeData("SpaceRefugee", "SECBC_SpaceRefugee"),
+                new BiocodeData("SpaceRefugee_Clothed", "SECBC_SpaceRefugee_Clothed"),
+                new BiocodeData("AncientSoldier", "SECBC_AncientSoldier"),
+
+                // Empire from PawnKinds_Empire.xml
+                new BiocodeData("Empire_Fighter_Trooper", "SECBC_Empire_Fighter_Trooper"),
+                new BiocodeData("Empire_Fighter_Janissary", "SECBC_Empire_Fighter_Janissary"),
+                new BiocodeData("Empire_Fighter_Champion", "SECBC_Empire_Fighter_Champion"),
+                new BiocodeData("Empire_Fighter_Cataphract", "SECBC_Empire_Fighter_Cataphract"),
+                new BiocodeData("Empire_Fighter_StellicGuardRanged", "SECBC_Empire_Fighter_StellicGuardRanged"),
+                new BiocodeData("Empire_Fighter_StellicGuardMelee", "SECBC_Empire_Fighter_StellicGuardMelee"),
+                new BiocodeData("Empire_Royal_NobleWimp", "SECBC_Empire_Royal_NobleWimp"),
+                new BiocodeData("Empire_Royal_Yeoman", "SECBC_Empire_Royal_Yeoman"),
+                new BiocodeData("Empire_Royal_Esquire", "SECBC_Empire_Royal_Esquire"),
+                new BiocodeData("Empire_Royal_Knight", "SECBC_Empire_Royal_Knight"),
+                new BiocodeData("Empire_Royal_Praetor", "SECBC_Empire_Royal_Praetor"),
+                new BiocodeData("Empire_Royal_Baron", "SECBC_Empire_Royal_Baron"),
+                new BiocodeData("Empire_Royal_Count", "SECBC_Empire_Royal_Count"),
+                new BiocodeData("Empire_Royal_Duke", "SECBC_Empire_Royal_Duke"),
+                new BiocodeData("Empire_Royal_Consul", "SECBC_Empire_Royal_Consul"),
+                new BiocodeData("Empire_Royal_Stellarch", "SECBC_Empire_Royal_Stellarch")
+            };
+
+            BCD_Other = new BiocodeData(null, "SECBC_Other");
+        }
+
+        public void SetAllValues(float newBiocodeChance)
+        {
+            BiocodeData.ForEach(d => d.BiocodeChance = newBiocodeChance);
+            BCD_Other.BiocodeChance = newBiocodeChance;
+        }
+    }
+}
---
+++ Source/SquirtingElephantSettings.cs
@@ -0,0 +1,93 @@
+﻿using SquirtingElephant.Helpers;
+using UnityEngine;
+using Verse;
+
+namespace SquirtingElephant.ConfigurableBiocoding
+{
+    public class SquirtingElephantSettings : Mod
+    {
+        #region Fields
+        
+        public static SettingsData Settings;
+
+        private const float SCROLL_AREA_OFFSET_TOP = 100f;
+        private const float ROW_HEIGHT = 32f;
+        private float SetAllChance = Consts.VANILLA_BIOCODE_CHANCE;
+
+        public static TableData Table = new TableData(new Vector2(10f, 0f), new Vector2(20f, 10f),
+            new float[] { 340f, 370f },
+            new float[] { ROW_HEIGHT },
+            2);
+
+        // Scroll settings.
+        private Vector2 ScrollPosition = Vector2.zero;
+        private Rect ViewRect = new Rect(0.0f, 0.0f, 100.0f, 200.0f);
+        
+        #endregion
+
+        public SquirtingElephantSettings(ModContentPack content) : base(content) { Settings = GetSettings<SettingsData>(); }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard ls = new Listing_Standard();
+            ls.Begin(inRect);
+
+            CreateTheAllSetting();
+
+            Widgets.Label(new Rect(10f, 20f + ROW_HEIGHT, inRect.width, ROW_HEIGHT), "SECBC_SettingInfo".TC());
+
+            ls.BeginScrollView(new Rect(0, SCROLL_AREA_OFFSET_TOP, inRect.width, inRect.height - SCROLL_AREA_OFFSET_TOP), ref ScrollPosition, ref ViewRect);
+
+            CreateHeaders();
+            CreateFields();
+            ls.GetRect(Table.Bottom); // Ensures the scrollview works.
+
+            ls.EndScrollView(ref ViewRect);
+            ls.End();
+
+            Main.ApplySettingsToDefs();
+        }
+
+        private void CreateTheAllSetting()
+        {
+            string bufferAllChance = SetAllChance.ToString();
+            Widgets.TextFieldNumericLabeled(new Rect(10f, 10f, 300f, ROW_HEIGHT), "SECBC_SetAllFields".TC() + " ", ref SetAllChance, ref bufferAllChance, Consts.BIOCODE_CHANCE_MIN, Consts.BIOCODE_CHANCE_MAX);
+            float count = Widgets.HorizontalSlider(new Rect(310f, 10f, 300f, ROW_HEIGHT), SetAllChance, Consts.BIOCODE_CHANCE_MIN, Consts.BIOCODE_CHANCE_MAX);
+            if (count != SetAllChance)
+            {
+                SetAllChance = count;
+            }
+            if (Widgets.ButtonText(new Rect(610f, 10f, 120f, ROW_HEIGHT), "SECBC_ApplyAll".TC()))
+            {
+                Settings.SetAllValues(SetAllChance);
+            }
+        }
+
+        private void CreateHeaders()
+        {
+            Widgets.Label(Table.GetHeaderRect(0), "SECBC_HeaderPawnKindName".TC());
+            Widgets.Label(Table.GetHeaderRect(1), "SECBC_HeaderBiocodeChance".TC());
+        }
+
+        private void CreateFields()
+        {
+            int rowIdx = 1;
+            foreach (BiocodeData biocodeData in Settings.BiocodeData)
+                MakeInputs(rowIdx++, biocodeData.SettingsTranslationKey, ref biocodeData.BiocodeChance, Consts.BIOCODE_CHANCE_MIN, Consts.BIOCODE_CHANCE_MAX);
+
+            MakeInputs(rowIdx++, Settings.BCD_Other.SettingsTranslationKey, ref Settings.BCD_Other.BiocodeChance, Consts.BIOCODE_CHANCE_MIN, Consts.BIOCODE_CHANCE_MAX);
+        }
+
+        private void MakeInputs(int rowIdx, string translationKey, ref float setting, float min, float max)
+        {
+            string buffer = setting.ToString();
+            Widgets.TextFieldNumericLabeled(Table.GetFieldRect(0, rowIdx), translationKey.TC() + " ", ref setting, ref buffer, min, max);
+
+            float count = Widgets.HorizontalSlider(Table.GetFieldRect(1, rowIdx), setting, min, max);
+            if (count != setting)
+                setting = count;
+        }
+
+        public override string SettingsCategory() => "SECBC_SettingsCategory".Translate();
+    }
+}
